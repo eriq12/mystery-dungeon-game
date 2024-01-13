28294d1bb6eae42636bcbf1f3b049065034c50da
Eriq <eriq12@protonmail.com>
2024-01-13 11:31:49 -0500

    modified library to be a scene

--- scripts/singletons/library.gd
+++ scripts/singletons/library.gd
@@ -4,4 +4,6 @@ var basic_move : CharacterMove = load("res://resources/moves/preset_moves/moveme
 
 var basic_face : CharacterMove = load("res://resources/moves/preset_moves/movement/face_direction_move.tres")
 
+
+
 @export var move_database : Array[CharacterMove]
